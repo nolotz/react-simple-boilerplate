3bede4754f37495ca183e44619f56fba7c280574
Monica Olinescu <monica.olinescu@gmail.com>
2016-07-17 22:49:32 -0700

    Switch back to class based component definition.
    Our tutorial asks students to go from this to createClass

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const App = React.createClass({
-  render: function() {
+class App extends Component {
+  render() {
     return (
       <h1>Hello React :)</h1>
     );
   }
-});
+}
 export default App;
